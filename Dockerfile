9ddab8fcfb64588abb81a2b3d6230aabf724d6ad
juliozambom <work.juliozambom@gmail.com>
2025-12-18 17:21:45 -0300

    feat: finish seeder using docker compose

--- Dockerfile
+++ Dockerfile
@@ -10,6 +10,8 @@ WORKDIR /app
 COPY ./requirements.txt /app/requirements.txt
 RUN pip install --no-cache-dir --upgrade -r /app/requirements.txt
 
+COPY ./seed_db.py /app/seed_db.py
 COPY ./app /app/app
+COPY ./data /app/data
 
 CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000"]
--- seed_db.py
+++ seed_db.py
@@ -1,8 +1,14 @@
 import os
 import geopandas as gpd
 from app.database import engine
+from sqlalchemy import inspect
+from shapely.geometry import MultiPolygon, Polygon
 
 def seed_db():
+    if inspect(engine).has_table("farms"):
+        print("Database is already populated, skiping seed script...")
+        return
+
     shapefile_path = "data/AREA_IMOVEL_1.shp"
 
     print("Looking if Shapefile exists...")
@@ -10,18 +16,31 @@ def seed_db():
     if not os.path.exists(shapefile_path):
         print(f"Error: {shapefile_path} not found!")
         return
-
-    print("Reading Shapefile...")
-
-    gdf = gpd.read_file(shapefile_path)
-
-    if gdf.crs != "EPSG:4326":
-        print("Converting coordinates to EPSG:4326...")
-        gdf = gdf.to_crs(epsg=4326)
-
-    print(f"Uploading {len(gdf)} farms to the database")
     
-    gdf.to_postgis("farms", engine, if_exists="replace", index=False)
+    chunk_size = 10000
+
+    total_records = len(gpd.read_file(shapefile_path, ignore_geometry=True, engine="pyogrio"))
+
+    print(f"Total farm records to populate in DB: {total_records}")
+
+    for start in range(0, total_records, chunk_size):
+        print(f"Processing farm records {start} to {min(start + chunk_size, total_records)}...")
+        
+        gdf = gpd.read_file(shapefile_path, rows=slice(start, start + chunk_size))
+
+        if gdf.crs != "EPSG:4326":
+            gdf = gdf.to_crs(epsg=4326)
+
+        mode = "replace" if start == 0 else "append"
+
+        gdf["geometry"] = [
+            MultiPolygon([feature]) if isinstance(feature, Polygon) else feature 
+            for feature in gdf["geometry"]
+        ]
+        
+        gdf.to_postgis("farms", engine, if_exists=mode, index=False)
+        
+        del gdf
     
     print("Database seeded successfully!")
 
