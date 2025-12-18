caac1db3643b61853b1159f490f46754d84da7ba
juliozambom <work.juliozambom@gmail.com>
2025-12-18 09:11:39 -0300

    feat: defining api base routes

--- app/main.py
+++ app/main.py
@@ -2,12 +2,25 @@ from typing import Union
 
 from fastapi import FastAPI
 
+from pydantic import BaseModel
+
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
 @app.get("/fazendas/{id}")
-def read_item(id: int, q: Union[str, None] = None):
+def get_farm(id: int, q: Union[str, None] = None):
     return {"id": id, "q": q}
+
+class Coordinate(BaseModel):
+    latitude: float
+    longitude: float
+
+@app.post("/fazendas/busca-ponto")
+def get_farm_by_coordinate(coord: Coordinate):
+    return {"id": 123, "coord": coord}
+
+class CoordinateAndRadius(Coordinate):
+    radius: int
+
+@app.post("/fazendas/busca-raio")
+def get_farm_by_radius(coord: CoordinateAndRadius):
+    return {"id": 123, "coord": coord}
